791084f1cacc7923f51b0deeae2253d929c897af
WebMaster <umrec_web@unimex.edu.mx>
2022-05-31 18:58:55 -0500

    config xs screen

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'xs': { 'min' : '250px', 'max': '639px'},
+      },
+      colors: {
+        'naranja-unimex' : '#f8981d'
+      }
+    }
   },
   plugins: [],
 }
