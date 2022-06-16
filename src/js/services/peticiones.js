68361d23f679677d15710c9160732bd3d317637d
WebMaster <umrec_web@unimex.edu.mx>
2022-06-16 17:57:19 -0500

    fetch petition to github api

--- src/js/services/peticiones.js
+++ src/js/services/peticiones.js
@@ -1,3 +1,8 @@
 /**
  * archivo para peticiones fetch...
  */
+export const getUser = async () => {
+
+    const response = await fetch(`https://api.github.com/users/artmcel`);
+    return  await response.json();
+};
