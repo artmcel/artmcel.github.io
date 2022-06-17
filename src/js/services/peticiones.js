409b3b9121fa455675ed76b1ab18b84e77f2b63a
WebMaster <umrec_web@unimex.edu.mx>
2022-06-17 16:26:04 -0500

    update some url

--- src/js/services/peticiones.js
+++ src/js/services/peticiones.js
@@ -12,31 +12,31 @@ const productionDevs = [
     {
         title : 'Aula UNIMEX',
         description : 'Landing page developed whit Angular v13, CSS, Bootstrap, GIT',
-        link : 'http://',
+        link : 'https://aula.unimex.edu.mx/',
     
     },
     {
         title : 'Día UNIMEX',
-        description : 'Landing page developed whit Angular v13, CSS, Bootstrap, GIT',
-        link : 'http://',
+        description : 'Registration form, with PHP, JS, CSS, Bootstrap, GIT',
+        link : 'https://unimex.edu.mx/dia-unimex/',
     
     },
     {
         title : 'Nuevo Ingreso',
-        description : 'Landing page developed whit Angular v13, CSS, Bootstrap, GIT',
-        link : 'http://',
+        description : 'Web application with PHP backend, JS, jQuery, CSS, Bootstrap, GIT',
+        link : 'https:/nuevoingreso.unimex.edu.mx/',
     
     },
     {
         title : 'Proyección Profesional',
-        description : 'Landing page developed whit Angular v13, CSS, Bootstrap, GIT',
-        link : 'http://',
+        description : 'Registration form, with PHP, JS, CSS, Bootstrap, GIT',
+        link : 'https://unimex.edu.mx/proyeccion-profesional/',
     
     },
     {
         title : 'Prospectación',
-        description : 'Landing page developed whit Angular v13, CSS, Bootstrap, GIT',
-        link : 'http://',
+        description : 'Registration form, with PHP, JS, CSS, Bootstrap, GIT',
+        link : 'https://unimexver.edu.mx/prospectacion/',
     
     }
 ];
@@ -47,43 +47,43 @@ const developmentDevs = [
     {
         title : 'Prospectación Update',
         description : 'This is Prospectacion project but with Webpack, I mean is the same project but bulding with other technology as Webpack the backend with PHP',
-        link : 'http://',
+        link : 'https://prueba.unimex.edu.mx/',
     
     },
     {
         title : 'otro update',
         description : 'Form project with Webpack and a Backend in PHP, here I do not use WS so I created a Object to consuming with JS and I send the data with fetch',
-        link : 'http://',
+        link : '',
     
     },
     {
         title : 'My webpack v5',
         description : 'This is my webpack config for webpack v5 projects',
-        link : 'http://',
+        link : 'https://github.com/artmcel/webpack-inicio',
     
     },
     {
         title : 'Bulding a SOAP',
         description : 'Bulding a soapCall and consuming with anonymous functions in PHP',
-        link : 'http://',
+        link : '',
     
     },
     {
         title : 'Backend whit Node',
         description : 'Simple backend with Node and Express and MongoDB',
-        link : 'http://',
+        link : 'https://github.com/artmcel/prueba2_backend',
     
     },
     {
         title : 'Local chat',
         description : 'Local real time chat with Node and Socket.io',
-        link : 'http://',
+        link : 'https://github.com/artmcel/Local-chat',
     
     },
     {
         title : 'Consuming API REST',
         description : 'A simple landign to get users from API REST',
-        link : 'http://',
+        link : '',
     
     }
 
