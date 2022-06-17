98c770010d926913b89d3333e8fc955f8460e4b5
WebMaster <umrec_web@unimex.edu.mx>
2022-06-17 11:04:30 -0500

    create tow funcitons to get data from devs objects and inject in html

--- src/js/componentes.js
+++ src/js/componentes.js
@@ -1,9 +1,12 @@
 /**
  * logica del proyecto
  */
+
 window.addEventListener('load', () => {
 
     getUser();
+    getDevs();
+    getProd();
     
 });
 
@@ -15,12 +18,70 @@ const getUser = async () => {
 
         user.then( ({avatar_url, ...all}) => {
 
-            console.log(all);
+            //console.log(all);
     
             const img = document.getElementById('img-profile');
 
-            img.src = avatar_url;
+            //img.src = avatar_url;
         });
+    }).catch(err => console.log(err));
+
+};
+
+const getProd = async () => {
+
+    await import('./services/peticiones').then( module => {
+
+        const devs = module.productionDevs,
+              productionDiv = document.getElementById('production-devs');
+        
+              
+        devs.forEach( ({title, description, link}) => {
+
+            const divs = `
+            <div class="m-2 px-1 text-center sm:flex-col sm:max-w-md md:max-w-lg lg:max-w-md">
+                <p class="h3 xs:text-base">${description}</p>
+                <button class="btn-proyectos">
+                    <a class="flex justify-center" href="${link}" target="_blank" rel="noopener noreferrer">
+                        ${title}
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 ml-1" viewBox="0 0 20 20" fill="currentColor">
+                            <path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z" />
+                            <path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z" />
+                        </svg>
+                    </a>
+                </button>
+            </div>`;	
+
+            productionDiv.insertAdjacentHTML('beforeend', divs);
+
+        });
+
+    }).catch(err => console.log(err));
+
+};
+
+const getDevs = async () => {
+
+    await import('./services/peticiones').then( module => {
+
+        const devs = module.developmentDevs,
+              personalDiv = document.getElementById('personal-devs');
+        
+              
+        devs.forEach( ({title, description, link}) => {
+
+            const divs = `
+            <div class="m-2 px-1 text-center sm:max-w-sm md:flex md:max-w-sm md:justify-self-center">
+                <p class="h3 xs:text-base md:w-2/3">${description}</p>
+                <button class="btn-learning h-fit self-center text-center p-2 md:w-1/3">
+                    <a href="${link}" target="_blank" rel="noopener noreferrer">${title}</a>
+                </button>
+            </div>`;
+
+            personalDiv.insertAdjacentHTML('beforeend', divs);
+
+        });
+
     }).catch(err => console.log(err));
 
 };
