96194aec669f00fd0da8895ba62c4750c0cf8f34
arturo <artmcel@hotmail.com>
2023-09-01 16:48:26 -0600

    add vite+react

---
+++ portafolio/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Footer from './layout/footer'
+import Header from './layout/header'
+import Main from './layout/main'
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <Main/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
