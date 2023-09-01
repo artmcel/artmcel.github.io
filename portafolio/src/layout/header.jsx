d262f5227314d976ebf686cdc3a7db6ac121afe7
arturo <artmcel@hotmail.com>
2023-09-01 16:48:47 -0600

    add hedaer

---
+++ portafolio/src/layout/header.jsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <header className="flex backdrop-blur-sm bg-[#f1ede5]/30">
+        <div className="w-full text-900 p-5 xs:text-center md:text-left">
+            <h1 className="h1 align-middle"></h1>
+        </div>
+    </header>
+  )
+}
