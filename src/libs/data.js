408f09d474005a60512466d5f2ba3686ba7e283c
arturo <artmcel@hotmail.com>
2023-09-01 18:01:43 -0600

    update de img url

--- src/libs/data.js
+++ src/libs/data.js
@@ -5,7 +5,7 @@ export const productionDevs = [
         title : 'Aula UNIMEX',
         description : 'Landing page desarrollada con Angular v13, CSS, Bootstrap, GIT',
         link : 'https://aula.unimex.edu.mx/',
-        imagen : './assets/aula.png'
+        imagen : './aula.png'
     
     },
     {
@@ -13,7 +13,7 @@ export const productionDevs = [
         title : 'Día UNIMEX',
         description : 'Formulario de registro desarrollado con PHP, JS, CSS, Bootstrap, GIT',
         link : 'https://unimex.edu.mx/dia-unimex/',
-        imagen: './assets/dia-unimex.png'
+        imagen: './dia-unimex.png'
     
     },
     {
@@ -21,7 +21,7 @@ export const productionDevs = [
         title : 'Nuevo Ingreso',
         description : 'Aplicación Web para mostrar videos de inducción, desarrollado con PHP, JS, jQuery, CSS, Bootstrap, GIT',
         link : 'https:/nuevoingreso.unimex.edu.mx/',
-        imagen: './assets/nuevo-ingreso.png'
+        imagen: './nuevo-ingreso.png'
     
     },
     {
@@ -29,7 +29,7 @@ export const productionDevs = [
         title : 'Proyección Profesional',
         description : 'Formulario de registro para prospectos de Posgrado desarrollado con PHP, JS, CSS, Bootstrap, GIT',
         link : 'https://unimex.edu.mx/proyeccion-profesional/',
-        imagen: './assets/proyeccion.png'
+        imagen: './proyeccion.png'
     
     },
     {
@@ -37,7 +37,7 @@ export const productionDevs = [
         title : 'Prospectación',
         description : 'Formulalrio para registro de prospectos, desarrollado con PHP, JS, CSS, Bootstrap, GIT',
         link : 'https://unimexver.edu.mx/prospectacion/',
-        imagen: './assets/prospectacion.png'
+        imagen: './prospectacion.png'
     
     },
     {
@@ -45,7 +45,7 @@ export const productionDevs = [
         title : 'Licenciaturas UNIMEX',
         description : 'Grupo de 8 formularios de registro conectadas con campañas de Google, desarrollados PHP, JS, CSS, Bootstrap, GIT, Webpack',
         link : 'https://landings.unimex.edu.mx/licenciatura-izcalli/',
-        imagen: './assets/landings.png'
+        imagen: './landings.png'
     
     }
 ];
@@ -58,7 +58,7 @@ export const developmentDevs = [
         title : 'Prospectación Update',
         description : 'Proyecto de prospectación pero ahora con Webpack, PHP y Bootstrap',
         link : 'https://github.com/artmcel/prospectacion',
-        imagen : './assets/prospectacion.png'
+        imagen : './prospectacion.png'
     
     },
     {
@@ -66,7 +66,7 @@ export const developmentDevs = [
         title : 'Proyeccion Update',
         description : 'Proyecto de proyección pero ahora con Webpack, PHP, Bootstrap',
         link : 'https://github.com/artmcel/proyeccion',
-        imagen : './assets/proyeccion.png'
+        imagen : './proyeccion.png'
     
     },
     {
@@ -74,7 +74,7 @@ export const developmentDevs = [
         title : 'Webpack v5',
         description : 'Mi configuración para crear aplicaciones web con WebPack v5',
         link : 'https://github.com/artmcel/webpack-inicio',
-        imagen : './assets/code.svg'
+        imagen : './code.svg'
     
     },
     {
@@ -82,7 +82,7 @@ export const developmentDevs = [
         title : 'Construyendo un SOAP Client',
         description : 'Consumiendo un WS con SoapClient en PHP 7.X',
         link : 'https://github.com/artmcel/soap',
-        imagen : './assets/code.svg'
+        imagen : './code.svg'
     
     },
     {
@@ -90,7 +90,7 @@ export const developmentDevs = [
         title : 'Backend en Node',
         description : 'Un backend con Node and Express and MongoDB',
         link : 'https://github.com/artmcel/prueba2_backend',
-        imagen : './assets/code.svg'
+        imagen : './code.svg'
     
     },
     {
@@ -98,7 +98,7 @@ export const developmentDevs = [
         title : 'Chat en tiempo real',
         description : 'Proyecto de un chat en tiempo real con Node and Socket.io y Bulba.io',
         link : 'https://github.com/artmcel/Local-chat',
-        imagen : './assets/code.svg'
+        imagen : './code.svg'
     
     },
     {
@@ -106,7 +106,7 @@ export const developmentDevs = [
         title : 'CRUD con Angular',
         description : 'Creando un CRUD con Angular, API REST ReqRes y Tailwind',
         link : 'https://artmcel.github.io/NGUserList/',
-        imagen : './assets/lista-usuarios.png'
+        imagen : './lista-usuarios.png'
     
     },
     {
@@ -114,7 +114,7 @@ export const developmentDevs = [
         title : 'CRUD con PHP y SQL Server',
         description : 'CRUD con PHP, SQLServer, Webpack, Bootstrap con una DB Local (examen de desarrollo web)',
         link : 'https://github.com/artmcel/agrega-alumnos',
-        imagen : './assets/code.svg'
+        imagen : './code.svg'
     
     },
     {
@@ -122,7 +122,7 @@ export const developmentDevs = [
         title : 'Formulario de registro página web',
         description : 'Formulario de registro de prospectos, desarrollado con PHP7, WebPack, Bootstrap',
         link : 'https://github.com/artmcel/registro-prosp',
-        imagen : './assets/registro-prosp.png'
+        imagen : './registro-prosp.png'
     
     },
     {
@@ -130,7 +130,7 @@ export const developmentDevs = [
         title : 'Graduaciones',
         description : 'Aplicación web para creación de boletos de graduación y registro de entrada, desarrollada con Angular, PHP, MySQL',
         link : 'https://github.com/artmcel/graduaciones',
-        imagen : './assets/code.svg'
+        imagen : './code.svg'
     
     }
 
