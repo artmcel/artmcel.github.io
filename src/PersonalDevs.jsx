86259ebae9c2205b6828a5ed1a7403a17724933b
arturo <artmcel@hotmail.com>
2023-09-22 17:59:42 -0600

    update title icons

--- src/PersonalDevs.jsx
+++ src/PersonalDevs.jsx
@@ -82,21 +82,7 @@ export default function PersonalDevs() {
         className="mx-auto md:w-2/4 lg:w-1/2"
         alt=""
       />
-      <h2 className="h2">
-        DESARROLLOS PERSONALES
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-6 w-6 ml-2 fill-orange-600"
-          viewBox="0 0 20 20"
-          fill="currentColor"
-        >
-          <path
-            fillRule="evenodd"
-            d="M2 5a2 2 0 012-2h12a2 2 0 012 2v2a2 2 0 01-2 2H4a2 2 0 01-2-2V5zm14 1a1 1 0 11-2 0 1 1 0 012 0zM2 13a2 2 0 012-2h12a2 2 0 012 2v2a2 2 0 01-2 2H4a2 2 0 01-2-2v-2zm14 1a1 1 0 11-2 0 1 1 0 012 0z"
-            clipRule="evenodd"
-          />
-        </svg>
-      </h2>
+      <h2 className="h2">DESARROLLOS PERSONALES 👨🏻‍💻🚀</h2>
 
       <div className="flex flex-col m-2">
         <p className='leading-realxed'>
