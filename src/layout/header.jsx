0fc323895dbfbae73e59bc6671393950b5c785f0
arturo <artmcel@hotmail.com>
2023-10-26 20:36:43 -0600

    update icons

--- src/layout/header.jsx
+++ src/layout/header.jsx
@@ -1,22 +1,24 @@
+import { Icon } from "@iconify/react";
+
 //import logo1 from '../assets/logo1.png';
 export default function Header() {
   return (
     <header className="bg-[#fffffe]">
       <nav className="flex flex-col md:flex-row">
         <div className="m-1 text-center basis-1/2 md:text-2xl">
-          <h1 className="font-sonsie">Arturo Morales</h1>
+          <h1 className="font-sonsie">Arturo Morales Celis</h1>
         </div>
-        <div className="m-1 text-center basis-1/2">
+        <div className="flex justify-center m-1 text-center basis-1/2">
           <a href="https://github.com/artmcel" target="_blank" rel="noopener noreferrer">
-            <box-icon type='logo' name='github'></box-icon>
+            <Icon icon="logos:github-octocat" height="24" width="48" className="m-3"/>
           </a>
 
           <a href="mailto:artmcel@hotmail.com" target="_blank" rel="noopener noreferrer">
-            <box-icon type='solid' name='envelope'></box-icon>
+            <Icon icon="logos:linkedin-icon" height="24" width="48" className="m-3"/>
           </a>
 
           <a href="https://www.linkedin.com/in/arturo-mor-celis/" target="_blank" rel="noopener noreferrer">
-            <box-icon type='logo' name='linkedin-square'></box-icon>
+            <Icon icon="logos:google-inbox" height="24" width="48" className="m-3"/>
           </a>
         </div>
       </nav>
