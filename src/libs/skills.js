a22cbf0628e4467c060509d0182581f996c223ca
arturo <artmcel@hotmail.com>
2023-10-26 20:10:28 -0600

    add skills object

---
+++ src/libs/skills.js
@@ -0,0 +1,46 @@
+export const skillsObject = [
+    {
+        name : 'HTML 5',
+        icon : 'html-5',
+    },
+    {
+        name : 'CSS3',
+        icon : 'css-3',
+    },
+    {
+        name : 'Bootstrap',
+        icon : 'bootstrap'
+    },
+    {
+        name : 'Tailwind',
+        icon : 'tailwindcss-icon'
+    },
+    {
+        name : 'JavaScript',
+        icon : 'javascript',
+    },
+    {
+        name : 'PHP',
+        icon : 'php',
+    },
+    {
+        name : 'Angular',
+        icon : 'angular-icon',
+    },
+    {
+        name : 'Laravel',
+        icon : 'laravel',
+    },
+    {
+        name: 'Webpack',
+        icon : 'webpack',
+    },
+    {
+        name: 'MySQL',
+        icon : 'mysql',
+    },
+    {
+        name: 'Git',
+        icon : 'git-icon',
+    }
+]
